2a3cd520f0eb9414ae717b6ee94388a6b537c19b
Abdul Dakkak <abduld@wolfram.com>
2017-06-02 03:06:15 -0400

    🔩 chore: add build go assets and icons to commit
    add build go assets and icons to commit

--- .cz-config.js
+++ .cz-config.js
@@ -1,27 +1,28 @@
 module.exports = {
   types: [
-    { value: "feat", name: "feat:     A new feature" },
-    { value: "fix", name: "fix:      A bug fix" },
-    { value: "docs", name: "docs:     Documentation only changes" },
+    { value: "🌟 feat", name: "🌟 feat:     A new feature" },
+    { value: "🐞 fix", name: "🐞 fix:      A bug fix" },
+    { value: "📚 docs", name: "📚 docs:     Documentation only changes" },
     {
-      value: "style",
-      name: "style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)"
+      value: "🎨 style",
+      name: "🎨 style:    Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)"
     },
     {
-      value: "refactor",
-      name: "refactor: A code change that neither fixes a bug nor adds a feature"
+      value: "✨ refactor",
+      name: "✨ refactor: A code change that neither fixes a bug nor adds a feature"
     },
     {
-      value: "perf",
-      name: "perf:     A code change that improves performance"
+      value: "⚡️ perf",
+      name: "⚡️ perf:     A code change that improves performance"
     },
-    { value: "test", name: "test:     Adding missing tests" },
+    { value: "✅ test", name: "✅ test:     Adding missing tests" },
     {
-      value: "chore",
-      name: "chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation"
+      value: "🔩 chore",
+      name: "🔩 chore:    Changes to the build process or auxiliary tools and libraries such as documentation generation"
     },
     { value: "revert", name: "revert:   Revert to a commit" },
-    { value: "WIP", name: "WIP:      Work in progress" }
+    { value: "🚨 WIP", name: "🚨 WIP:      Work in progress" },
+    { value: "📦 Release", name: "Release: Release a new version" }
   ],
 
   scopes: [
---
+++ scripts/build_go_assets.js
@@ -0,0 +1,117 @@
+/*
+ * Minio Browser (C) 2016 Minio, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var moment = require("moment");
+var async = require("async");
+var exec = require("child_process").exec;
+var fs = require("fs");
+var rimraf = require("rimraf");
+
+var isProduction = true;
+var assetsFileName = "";
+var commitId = "";
+var date = moment.utc();
+var version = date.format("YYYY-MM-DDTHH:mm:ss") + "Z";
+var releaseTag = date.format("YYYY-MM-DDTHH-mm-ss") + "Z";
+var buildType = "production";
+var assetsFileName = "../server/bindata_assetfs.go";
+if (process.env.IO_UI_BUILD) buildType = process.env.IO_UI_BUILD;
+
+async.waterfall(
+  [
+    function(cb) {
+      rimraf.sync(assetsFileName);
+      rimraf.sync("build");
+      var cmd = "yarn build";
+      console.log("Running", cmd);
+      exec(cmd, cb);
+    },
+    function(stdout, stderr, cb) {
+      var cmd = 'git log --format="%H" -n1';
+      console.log("Running", cmd);
+      exec(cmd, cb);
+    },
+    function(stdout, stderr, cb) {
+      if (!stdout) throw new Error("commitId is empty");
+      commitId = stdout.replace("\n", "");
+      if (commitId.length !== 40)
+        throw new Error("commitId invalid : " + commitId);
+      var cmd =
+        "go-bindata-assetfs -pkg server -nocompress=false -o " +
+        assetsFileName +
+        " ../build/...";
+      console.log(cmd);
+      console.log("Running", cmd);
+      exec(cmd, cb);
+    },
+    function(stdout, stderr, cb) {
+      var cmd = "gofmt -s -w -l " + assetsFileName;
+      console.log("Running", cmd);
+      exec(cmd, cb);
+    },
+    function(stdout, stderr, cb) {
+      fs.appendFileSync(assetsFileName, "\n");
+      fs.appendFileSync(
+        assetsFileName,
+        'var UIReleaseTag = "' + releaseTag + '"\n'
+      );
+      fs.appendFileSync(
+        assetsFileName,
+        'var UIBuildType = "' + buildType + '"\n'
+      );
+      fs.appendFileSync(
+        assetsFileName,
+        'var UICommitID = "' + commitId + '"\n'
+      );
+      fs.appendFileSync(assetsFileName, 'var UIVersion = "' + version + '"');
+      fs.appendFileSync(assetsFileName, "\n");
+      var contents = fs.readFileSync(assetsFileName, "utf8");
+      // if (isProduction) {
+      // 	contents = contents
+      // 		.replace(/_productionIndexHtml/g, '_productionIndexHTML')
+      // 		.replace(/productionIndexHtmlBytes/g, 'productionIndexHTMLBytes')
+      // 		.replace(/productionIndexHtml/g, 'productionIndexHTML')
+      // 		.replace(/_productionIndex_bundleJs/g, '_productionIndexBundleJs')
+      // 		.replace(/productionIndex_bundleJsBytes/g, 'productionIndexBundleJsBytes')
+      // 		.replace(/productionIndex_bundleJs/g, 'productionIndexBundleJs')
+      // 		.replace(/_productionJqueryUiMinJs/g, '_productionJqueryUIMinJs')
+      // 		.replace(/productionJqueryUiMinJsBytes/g, 'productionJqueryUIMinJsBytes')
+      // 		.replace(/productionJqueryUiMinJs/g, 'productionJqueryUIMinJs');
+      // } else {
+      // 	contents = contents
+      // 		.readFileSync(assetsFileName, 'utf8')
+      // 		.replace(/_assetsIndexHtml/g, '_assetsIndexHTML')
+      // 		.replace(/assetsIndexHtmlBytes/g, 'assetsIndexHTMLBytes')
+      // 		.replace(/assetsIndexHtml/g, 'assetsIndexHTML')
+      // 		.replace(/_assetsIndex_bundleJs/g, '_assetsIndexBundleJs')
+      // 		.replace(/assetsIndex_bundleJsBytes/g, 'assetsIndexBundleJsBytes')
+      // 		.replace(/assetsIndex_bundleJs/g, 'assetsIndexBundleJs')
+      // 		.replace(/_assetsJqueryUiMinJs/g, '_assetsJqueryUIMinJs')
+      // 		.replace(/assetsJqueryUiMinJsBytes/g, 'assetsJqueryUIMinJsBytes')
+      // 		.replace(/assetsJqueryUiMinJs/g, 'assetsJqueryUIMinJs');
+      // }
+      contents = contents.replace(/UI_VERSION/g, version);
+      contents = contents.replace(/main_bundle.js/g, "main_bundle.js");
+
+      fs.writeFileSync(assetsFileName, contents, "utf8");
+      console.log("UI assets file :", assetsFileName);
+      cb();
+    }
+  ],
+  function(err) {
+    if (err) return console.log(err);
+  }
+);
