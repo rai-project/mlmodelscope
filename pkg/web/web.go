07c99188857d89085814e2c144a52cc2afbd01dc
Abdul Dakkak <abduld@wolfram.com>
2017-06-04 21:48:16 +0100

    feat: use gzip middleware

--- pkg/web/web.go
+++ pkg/web/web.go
@@ -2,15 +2,11 @@ package web
 
 import (
 	"fmt"
-	"io"
 	"strings"
 
-	"github.com/Sirupsen/logrus"
-	"github.com/davecgh/go-spew/spew"
 	"github.com/k0kubun/pp"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	glog "github.com/labstack/gommon/log"
 )
 
 func Start(addr string) {
@@ -26,6 +22,9 @@ func Start(addr string) {
 		Output:  log.Writer(),
 	}))
 	e.Use(middleware.Recover())
+	e.Use(middleware.GzipWithConfig(middleware.GzipConfig{
+		Level: 5,
+	}))
 
 	if err := assetsRoutes(e); err != nil {
 		panic(err)
@@ -63,47 +62,4 @@ func pprintAddr(addr string) string {
 		}
 	}
 	return strings.Join(parts, ":")
-}
-
-type echoLogger struct {
-	*logrus.Entry
-}
-
-func (l *echoLogger) SetPrefix(string) {
-}
-func (l *echoLogger) Prefix() string {
-	return ""
-}
-func (l *echoLogger) SetLevel(level glog.Lvl) {
-	l.Entry.Level = logrus.Level(level)
-}
-func (l *echoLogger) Level() glog.Lvl {
-	return glog.Lvl(l.Entry.Level)
-}
-func (l *echoLogger) Output() io.Writer {
-	return l.Entry.Writer()
-}
-func (l *echoLogger) SetOutput(w io.Writer) {
-	l.Entry.Logger.Out = w
-}
-func (l *echoLogger) Printj(msg glog.JSON) {
-	l.Entry.Print(spew.Sprint(msg))
-}
-func (l *echoLogger) Debugj(msg glog.JSON) {
-	l.Entry.Debug(spew.Sprint(msg))
-}
-func (l *echoLogger) Infoj(msg glog.JSON) {
-	l.Entry.Info(spew.Sprint(msg))
-}
-func (l *echoLogger) Errorj(msg glog.JSON) {
-	l.Entry.Error(spew.Sprint(msg))
-}
-func (l *echoLogger) Warnj(msg glog.JSON) {
-	l.Entry.Warn(spew.Sprint(msg))
-}
-func (l *echoLogger) Fatalj(msg glog.JSON) {
-	l.Entry.Fatal(spew.Sprint(msg))
-}
-func (l *echoLogger) Panicj(msg glog.JSON) {
-	l.Entry.Panic(spew.Sprint(msg))
 }
