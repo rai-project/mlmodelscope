10a1f3614c4920e19d5974b2f82973117126a6fd
Carl Pearson <pearson@illinois.edu>
2017-06-08 19:25:42 -0400

    Add some fixup to generated js from protogen.sh

--- scripts/protogen.sh
+++ scripts/protogen.sh
@@ -17,3 +17,13 @@ protoc \
   ../../../github.com/rai-project/dlframework/mxnet/mxnet.proto
 
 echo "**/*js" >> ./src/proto/.eslintignore
+
+# Insert "/* eslint-disable */"
+for f in $(find ./src/proto -name "*.js"); do
+  sed -i '1i /* eslint-disable */' $f
+done
+for f in $(find ./src/proto -type f); do
+  cat $f | grep "var github\.com"
+  sed -i -e 's/var github.com/var github_com/g' $f
+  cat $f | grep "var github_com"
+done
