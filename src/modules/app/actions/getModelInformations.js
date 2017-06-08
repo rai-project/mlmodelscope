52d1892a91254ae8413fafa8333c7f7530c37a58
Abdul Dakkak <abduld@wolfram.com>
2017-06-09 00:50:20 +0100

    chore: silence warnings

--- src/modules/app/actions/getModelInformations.js
+++ src/modules/app/actions/getModelInformations.js
@@ -1,26 +1,7 @@
 import { grpc } from "grpc-web-client";
 
 import { MXNet } from "../../../proto/github.com/rai-project/dlframework/mxnet/mxnet_pb_service";
-import {
-  ModelInformations,
-  MXNetModelInformationRequest,
-  Null
-} from "../../../proto/github.com/rai-project/dlframework/mxnet/mxnet_pb";
-
-function getModelInformations0({ state, uuid, controller, props }) {
-  var req = new Null();
-  grpc.invoke(MXNet.GetModelInformations, {
-    request: req,
-    host: "/api/mxnet",
-    onMessage: message => {
-      console.log({ onMessage: message });
-    },
-    onEnd: (code, message, trailers) => {
-      console.log("onEnd", { code, message, trailers });
-    },
-    debug: true
-  });
-}
+import { MXNetModelInformationRequest } from "../../../proto/github.com/rai-project/dlframework/mxnet/mxnet_pb";
 
 function getModelInformations({ state, uuid, controller, props }) {
   var req = new MXNetModelInformationRequest();
