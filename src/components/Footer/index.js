d31b2bf4f25648b44c53f70c35ad4bbe6cba2f75
Abdul Dakkak <abduld@wolfram.com>
2017-06-12 12:18:54 +0100

    chore: make images smaller

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Header, Icon, Image } from "semantic-ui-react";
 
-import carmlImage from "../../assets/images/carml.png";
+import carmlImage from "../../assets/images/carml_small.png";
 import c3srImage from "../../assets/images/C3SR_color.jpg";
-import uiucImage from "../../assets/images/UIUC_Logo_University_of_Illinois_at_Urbana-Champaign.jpg";
+import uiucImage from "../../assets/images/uiuc_logo_small.png";
 
 export default function Footer() {
   return (
