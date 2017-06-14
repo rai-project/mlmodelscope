0feb2f70c16aa5013e5f9406bbac90c6256ca8f4
abdul dakkak <abduld@wolfram.com>
2017-06-14 15:38:14 +0100

    Create install-protobuf.sh

--- scripts/install-protobuf.sh
+++ scripts/install-protobuf.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
 # check to see if protobuf folder is empty
-if [ ! -d "$HOME/protobuf" ]; then
+if [ ! -d "$HOME/protobuf/bin" ]; then
   wget https://github.com/google/protobuf/releases/download/v$PROTOBUF_VER/protoc-$PROTOBUF_VER-linux-x86_64.zip
   unzip protoc-$PROTOBUF_VER-linux-x86_64.zip -o -d $HOME/protobuf/
 else
