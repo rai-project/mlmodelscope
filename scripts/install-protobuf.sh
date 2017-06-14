fdf5d51d3d0480f9c665991e485b3cb788ff77ee
Abdul Dakkak <abduld@wolfram.com>
2017-06-14 18:01:48 +0100

    chore: make dir

--- scripts/install-protobuf.sh
+++ scripts/install-protobuf.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -ex
 wget https://github.com/google/protobuf/releases/download/v$PROTOBUF_VER/protoc-$PROTOBUF_VER-linux-x86_64.zip
+mkdir -p $HOME/protobuf/
 unzip protoc-$PROTOBUF_VER-linux-x86_64.zip -o -d $HOME/protobuf/
