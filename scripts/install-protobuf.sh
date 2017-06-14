861b00e653dbc7afefc442f47f26ed6a93fc5776
Abdul Dakkak <abduld@wolfram.com>
2017-06-14 18:07:50 +0100

    fix: typo

--- scripts/install-protobuf.sh
+++ scripts/install-protobuf.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 set -ex
 wget https://github.com/google/protobuf/releases/download/v$PROTOBUF_VER/protoc-$PROTOBUF_VER-linux-x86_64.zip
-mkdir -p $HOME/protobuf/
-unzip protoc-$PROTOBUF_VER-linux-x86_64.zip -o -d $HOME/protobuf/
+unzip protoc-$PROTOBUF_VER-linux-x86_64.zip -d $HOME/protobuf/
