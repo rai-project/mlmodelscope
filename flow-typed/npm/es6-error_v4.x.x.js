fddf48945d6ff1b1495efbaac6af88a895e95cb8
Abdul Dakkak <abduld@wolfram.com>
2017-06-29 13:06:08 +0100

    add flow

---
+++ flow-typed/npm/es6-error_v4.x.x.js
@@ -0,0 +1,8 @@
+// flow-typed signature: 98ff00b9e97d6ea73f700f11d9f9b3fe
+// flow-typed version: 6a420396a2/es6-error_v4.x.x/flow_>=v0.46.x
+
+declare module "es6-error" {
+  declare class ExtendableError extends Error {}
+
+  declare var exports: Class<ExtendableError>;
+}
