734a4fd9c268682a40a783fa6e222d9ea95b761c
Abdul Dakkak <abduld@wolfram.com>
2017-06-29 13:33:31 +0100

    stye lint

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import controller from "./controller";
 import { Container } from "cerebral/react";
 import registerServiceWorker from "./registerServiceWorker";
 
-import "./reset.css";
 import "semantic-ui-css/semantic.min.css";
-import "./styles.css";
 
 import App from "./components/App";
 
