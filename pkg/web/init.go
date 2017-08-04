ced2d7b922b6521279252063a4d50b06dd930557
abdul dakkak <abduld@wolfram.com>
2017-08-04 04:32:57 -0400

    Update init.go

--- pkg/web/init.go
+++ pkg/web/init.go
@@ -3,8 +3,6 @@ package web
 import (
 	"github.com/rai-project/config"
 	"github.com/rai-project/logger"
-	"github.com/rai-project/tracer"
-	"github.com/rai-project/tracer/zipkin"
 	"github.com/sirupsen/logrus"
 )
 
@@ -15,14 +13,6 @@ var (
 func init() {
 	config.AfterInit(func() {
 		log = logger.New().WithField("pkg", "carml/web")
-		if tracer.Enabled() && tracer.Backend() == "zipkin" {
-			t, err := zipkin.New("webserver")
-			if err != nil {
-				log.WithError(err).Error("failed to create a new zipkin tracer")
-				return
-			}
-			tracer.SetStd(t)
-		}
 	})
 
 }
