bc3f411cad74367fd387a8540b06d176aaa55a7a
Abdul Dakkak <abduld@wolfram.com>
2017-08-08 20:52:37 -0500

    work

--- pkg/web/api.go
+++ pkg/web/api.go
@@ -2,9 +2,6 @@ package web
 
 import (
 	"net/http"
-	"strings"
-
-	"google.golang.org/grpc"
 
 	"github.com/labstack/echo"
 	"github.com/rai-project/config"
@@ -38,30 +35,5 @@ func apiRoutes(e *echo.Echo) error {
 	)
 	api.Any("/*", StripPrefix("/api", echo.WrapHandler(dlframeworkHandler)))
 
-	// mux := runtime.NewServeMux()
-	// opts := []grpc.DialOption{grpc.WithInsecure()}
-
-	// ctx, cancel := context.WithCancel(context.Background())
-	// defer cancel()
-	// err = dlframework.RegisterRegistryHandlerFromEndpoint(ctx, mux, "localhost", opts)
-	// if err != nil {
-	// 	return err
-	// }
-
-	// api.Any("/*", StripPrefix("/api", echo.WrapHandler(mux)))
 	return nil
-}
-
-// grpcHandlerFunc returns an http.Handler that delegates to grpcServer on incoming gRPC
-// connections or otherHandler otherwise. Copied from cockroachdb.
-func grpcHandlerFunc(grpcServer *grpc.Server, otherHandler http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// TODO(tamird): point to merged gRPC code rather than a PR.
-		// This is a partial recreation of gRPC's internal checks https://github.com/grpc/grpc-go/pull/514/files#diff-95e9a25b738459a2d3030e1e6fa2a718R61
-		if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
-			grpcServer.ServeHTTP(w, r)
-		} else {
-			otherHandler.ServeHTTP(w, r)
-		}
-	})
 }
