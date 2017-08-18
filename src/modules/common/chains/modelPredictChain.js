66cb103c933e9831b91e566627454a8756676a04
Abdul Dakkak <abduld@wolfram.com>
2017-08-18 15:49:01 +0100

    initial work on multi-model

--- src/modules/common/chains/modelPredictChain.js
+++ src/modules/common/chains/modelPredictChain.js
@@ -1,3 +1,4 @@
+import { parallel } from "cerebral";
 import { set, when } from "cerebral/operators";
 import { state, props } from "cerebral/tags";
 import { head } from "lodash";
@@ -5,19 +6,36 @@ import base64 from "../compute/base64";
 import onError from "./onError";
 import { Predict } from "../../../swagger/dlframework";
 
-function a({ props }) {
-  console.log(arguments);
-  let model = head(props.selectedModels);
-  return Predict({
-    body: {
-      framework_name: model.framework.name,
-      framework_version: model.framework.version,
-      model_name: model.name,
-      model_version: model.version,
-      data: base64(state`app.predictURL`),
-      limit: 10,
-    },
-  });
+function predict(predictURL, selectedModels) {
+  return ({ http, path, resolve }) => {
+    const models = resolve.value(selectedModels);
+    const url = resolve.value(predictURL);
+    console.log(url);
+    console.log(path);
+    return Promise.all(
+      models.map(model => {
+        return Predict({
+          body: {
+            framework_name: model.framework.name,
+            framework_version: model.framework.version,
+            model_name: model.name,
+            model_version: model.version,
+            data: base64(url),
+            limit: 10
+          }
+        })({
+          http,
+          path,
+          resolve
+        });
+      })
+    )
+      .then(path.success)
+      .catch(function(output) {
+        console.log("on error");
+        path.error(head(output));
+      });
+  };
 }
 
 export default [
@@ -30,13 +48,13 @@ export default [
   {
     true: [
       set(state`app.isPredicting`, true),
-      a,
-      // {
-      //   success: [set(state`app.features`, props`result.features`)],
-      //   error: onError,
-      // },
-      set(state`app.isPredicting`, false),
+      predict(state`app.predictURL`, props`selectedModels`),
+      {
+        success: [set(state`app.features`, props`result.features`)],
+        error: onError
+      },
+      set(state`app.isPredicting`, false)
     ],
-    false: [],
-  },
+    false: []
+  }
 ];
