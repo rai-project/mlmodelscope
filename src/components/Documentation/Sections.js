f3f8bee3f9bcf879ee35b7f34d649172140dc5c1
Abdul Dakkak <abduld@wolfram.com>
2017-08-27 10:20:34 +0100

    make about page render documentation pages

---
+++ src/components/Documentation/Sections.js
@@ -0,0 +1,34 @@
+/* eslint-disable */
+
+export {
+  default as Introduction
+} from "!raw-loader!../../assets/docs/introduction.md";
+export { default as Usage } from "!raw-loader!../../assets/docs/usage.md";
+export { default as Concepts } from "!raw-loader!../../assets/docs/concepts.md";
+export {
+  default as Architecture
+} from "!raw-loader!../../assets/docs/architecture.md";
+export {
+  default as DeepLearning
+} from "!raw-loader!../../assets/docs/deeplearning.md";
+export { default as Backend } from "!raw-loader!../../assets/docs/backend.md";
+export {
+  default as WebServer
+} from "!raw-loader!../../assets/docs/webserver.md";
+export { default as WebUI } from "!raw-loader!../../assets/docs/webui.md";
+export {
+  default as Prediction
+} from "!raw-loader!../../assets/docs/prediction.md";
+export {
+  default as Installation
+} from "!raw-loader!../../assets/docs/installation.md";
+export {
+  default as Configuration
+} from "!raw-loader!../../assets/docs/configuration.md";
+export { default as API } from "!raw-loader!../../assets/docs/api.md";
+export {
+  default as Competitors
+} from "!raw-loader!../../assets/docs/competitors.md";
+export {
+  default as References
+} from "!raw-loader!../../assets/docs/references.md";
--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -1,11 +1,7 @@
 import React from "react";
 import { connect } from "cerebral/react";
-import { Header } from "semantic-ui-react";
+import { Section } from "../Documentation";
 
 export default connect({}, function AboutPage() {
-  return (
-    <a href="https://rai-project.github.io/carml">
-      <Header>TODO</Header>
-    </a>
-  );
+  return <Section name="Introduction" />;
 });
