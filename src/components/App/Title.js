d0537afedfede3cda4ca61bb127cac7f95a39087
Abdul Dakkak <abduld@wolfram.com>
2018-02-01 11:29:32 -0600

    work

---
+++ src/components/App/Title.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { connect } from '@cerebral/react'
+import { state } from 'cerebral/tags'
+
+import { Header as UIHeader, Container } from 'semantic-ui-react'
+
+const fontFamily = '"Raleway", "Helvetica Neue", Helvetica, Arial, sans-serif'
+
+export default connect(
+  {
+    currentPage: state`app.currentPage`,
+    appName: state`app.name`,
+  },
+  ({ appName, currentPage }) => {
+    const showTagLine = currentPage === 'Home'
+    return (
+      <div data-tut="main-header" className="App-header">
+        <Container textAlign={'center'}>
+          <UIHeader
+            inverted
+            size="huge"
+            style={{
+              fontFamily,
+            }}
+          >
+            {appName}
+          </UIHeader>
+          {showTagLine ? (
+            <UIHeader
+              inverted
+              size="small"
+              style={{
+                fontFamily,
+              }}
+            >
+              Cognitive ARtifacts for Machine Learning
+            </UIHeader>
+          ) : null}
+        </Container>
+      </div>
+    )
+  }
+)
