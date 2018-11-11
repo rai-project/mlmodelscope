dd160bb0a16cd6ccad1262358d086a94b1266fb2
Abdul Dakkak <abduld@wolfram.com>
2018-11-11 08:24:22 -0600

    update

--- pkg/jaeger.go
+++ pkg/jaeger.go
@@ -5,10 +5,6 @@ import (
 	"github.com/rai-project/web/jaegerui"
 )
 
-func jaegerAssets2(e *echo.Echo) error {
-	return jaegerui.Routes(e, "jaeger")
-}
-
 func jaegerAssets(e *echo.Echo) error {
 	e.Any("/jaeger/*", jaegerui.Handle())
 	return nil
