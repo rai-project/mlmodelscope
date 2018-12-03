ce6df585edbed36fb75e994621a4596ad28f111b
Renee Zhu <reneezhu1994@gmail.com>
2018-12-03 13:23:45 -0600

    adjust footer to be compatible on phone

--- src/components/GlobalFooter/index.js
+++ src/components/GlobalFooter/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./GlobalFooter.css";
-import { Layout } from "antd";
+import { Layout, Row, Col } from "antd";
 import C3SRLogo from "../../resources/logo/logo-c3sr.png";
 import IBMLogo from "../../resources/logo/logo-ibm.svg";
 import UIUCLogo from "../../resources/logo/logo-uiuc.svg";
@@ -17,34 +17,38 @@ export default class GlobalFooter extends Component {
           padding: "0px",
         }}
       >
-        <div style={{ display: "inline", float: "left", paddingTop: "20px" }}>
-          <div className="GlobalFooter-block">
-            <a href="https://c3sr.com">
-              <img src={C3SRLogo} className="C3SR-logo" alt="" />
-            </a>
-          </div>
-          <div className="GlobalFooter-block">
+      <Row>
+        <Col sm={{span: 4}} xs={{span: 8}}>
+          <a href="https://c3sr.com">
+            <img src={C3SRLogo} className="C3SR-logo" alt="" />
+          </a>
+        </Col>
+        <Col sm={{span: 2}} xs={{span: 4}}>
             <a href="https://github.com/rai-project" style={{ color: "white" }}>
               GitHub
             </a>
-          </div>
-          <div className="GlobalFooter-block">Terms</div>
-          <div className="GlobalFooter-block">Privacy</div>
-          <div className="GlobalFooter-block">Copyright</div>
-        </div>
-        <div style={{ display: "inline", float: "right", paddingTop: "40px" }}>
-          <div className="GlobalFooter-block-right">
+        </Col>
+        <Col sm={{span: 2}} xs={{span: 4}} style={{ color: "white", height: "auto" }}>
+          Terms
+        </Col>
+        <Col sm={{span: 2}} xs={{span: 4}} style={{ color: "white" }}>
+          Privacy
+        </Col>
+        <Col sm={{span: 2}} xs={{span: 4}} style={{ color: "white" }}>
+          Copyright
+        </Col>
+        <Col sm={{span: 8}} xs={{span: 24}} style={{height: "10px"}}/>
+        <Col sm={{span: 2}} xs={{span: 4}} style={{ color: "white" }}>
             <a href="https://ibm.com">
-              {" "}
               <img src={IBMLogo} className="IBM-logo" alt="" />
             </a>
-          </div>
-          <div className="GlobalFooter-block-right">
+        </Col>
+        <Col sm={{span: 2}} xs={{span: 4}} style={{ color: "white" }}>
             <a href="http://impact.crhc.illinois.edu">
               <img src={UIUCLogo} className="UIUC-logo" alt="" />
             </a>
-          </div>
-        </div>
+        </Col>
+      </Row>
       </Footer>
     );
   }
