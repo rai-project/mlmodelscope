bed24a309cbb21859cfa3aa9226c4ab1031d7656
Abdul Dakkak <abduld@wolfram.com>
2019-04-09 15:00:48 -0500

    continue work on detailed features

--- src/components/LandingPage/DetailedFeatures.js
+++ src/components/LandingPage/DetailedFeatures.js
@@ -28,30 +28,31 @@ const FeatureRow = function({ isMobile, children }) {
   );
 };
 
-const FeatureCard = function() {
+const FeatureCard = function({ title, children }) {
   return (
-    <Col lg={8} md={16} sm={24} style={{ paddingTop: "10px", paddingBottom: "10px" }}>
+    <Col lg={8} md={16} sm={24} style={{ padding: "10px" }}>
       <Card
         hoverable
-        bordered={false}
-        title={
-          <div style={{ fontSize: "2rem", color: TextColor }}>System Architects</div>
-        }
-      >
-        <Card.Meta
-          description={
-            <div
-              style={{
-                color: "#1A263A",
-                fontWeight: 400,
-                fontSize: "1.5rem",
-                color: TextColor,
-              }}
-            >
-              Analyse ML workloads across systems, accelerators, and software stacks
-            </div>
+        style={
+          {
+            // borderTop: "0",
+            // borderLeft: "0",
+            // borderRight: "0",
+            // borderBottom: "0",
           }
-        />
+        }
+        headStyle={{
+          fontSize: "2rem",
+          color: TextColor,
+        }}
+        bodyStyle={{
+          fontWeight: 400,
+          fontSize: "1.5rem",
+          color: TextColor,
+        }}
+        title={title}
+      >
+        <Card.Meta description={children} />
       </Card>
     </Col>
   );
@@ -66,14 +67,38 @@ class DetailedFeatures extends Component {
       <React.Fragment>
         <PanelsHeading>Features</PanelsHeading>
         <FeatureRow isMobile={isMobile}>
-          <FeatureCard />
-          <FeatureCard />
-          <FeatureCard />
+          <FeatureCard title={"Frameworks"}>
+            Publish your models or systems to the MLModelScope and exposed them through
+            the online hub to gather feedback such as realistic accuracy results and
+            failure points.
+          </FeatureCard>
+          <FeatureCard title={"Models"}>
+            MLModelScope contains hundreds of builtin models. These models span different
+            frameworks and allow one to evaluate accuracy, performance, and energy across
+            frameworks.
+          </FeatureCard>
+          <FeatureCard title={"Systems"}>
+            MLModelScope has been tested on Linux, Android, iOS, and macOS running on X86,
+            ARM, and PowerPC CPU architectures and leverage accelerators such as GPUs and
+            FPGAs.
+          </FeatureCard>
         </FeatureRow>
         <FeatureRow isMobile={isMobile}>
-          <FeatureCard />
-          <FeatureCard />
-          <FeatureCard />
+          <FeatureCard title={"Publish Your Workflow"}>
+            Publish your models or systems to the MLModelScope and exposed them through
+            the online hub to gather feedback such as realistic accuracy results and
+            failure points.
+          </FeatureCard>
+          <FeatureCard title={"Workflow Analysis"}>
+            Publish your models or systems to the MLModelScope and exposed them through
+            the online hub to gather feedback such as realistic accuracy results and
+            failure points.
+          </FeatureCard>
+          <FeatureCard title={"Workflow Analysis"}>
+            Publish your models or systems to the MLModelScope and exposed them through
+            the online hub to gather feedback such as realistic accuracy results and
+            failure points.
+          </FeatureCard>
         </FeatureRow>
       </React.Fragment>
     );
