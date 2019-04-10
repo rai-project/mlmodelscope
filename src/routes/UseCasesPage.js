ab6254281ab384d92febda084d2d7b4f59b55053
Abdul Dakkak <abduld@wolfram.com>
2019-04-10 14:58:36 -0500

    update about/user page

--- src/routes/UseCasesPage.js
+++ src/routes/UseCasesPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Markdown from "react-markdown";
-import { Layout } from "antd";
+import { Row, Col, Layout } from "antd";
 import Helmet from "react-helmet";
 
 // eslint-disable-next-line
@@ -11,7 +11,11 @@ export default class UseCasesPage extends Component {
     return (
       <Layout.Content style={{ marginLeft: "40px", padding: "30px" }}>
         <Helmet title="About" meta={[{ property: "og:title", content: "About" }]} />
-        <Markdown source={AboutContent} />
+        <Row type="flex" justify="center" style={{ paddingTop: "30px" }}>
+          <Col xlg={8} lg={16} sm={20}>
+            <Markdown source={AboutContent} />
+          </Col>
+        </Row>
       </Layout.Content>
     );
   }
